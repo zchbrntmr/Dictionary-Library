c8e7c62208ec828b32cc9d041c77cbb833e250e5
Zach <brantmeierz@ww-whippets.org>
2013-09-23 18:37:46 -0500

    Added much more contents, and working functions.  Many more word support to come later.

--- dictFunc.js
+++ dictFunc.js
@@ -1,3 +1,33 @@
-function isNoun(checking){
-  
+function isNoun(checkFor){
+  checkFor.toLowerCase();
+  var nouns=["cat","dog","elephant","ruler","xylophone","yacht","zebra","zygote"];
+  for(var i=-1;i<=nouns.length;i++){
+    if(nouns[i]==checkFor){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+function isAdjective(checkFor){
+  checkFor.toLowerCase();
+  var adjectives=["creative","funny","interesting","smart"];
+  for(var i=-1;i<=adjectives.length;i++){
+    if(adjectives[i]==checkFor){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
+function isName(checkFor){
+  checkFor.toLowerCase();
+  var names=["alex","andrew","amy","bill","justin","steve","steven","will","william","zach","zachary"];
+  for(var i=0;i<=names.length;i++){
+    if(names[i]==checkFor){
+      return true;
+    }else{
+      return false;
+    }
+  }
 }
