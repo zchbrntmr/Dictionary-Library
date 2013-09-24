d396d3e1e61de902a83cfba93539285cf1560d5f
Zach <brantmeierz@ww-whippets.org>
2013-09-23 19:16:02 -0500

    Improvements and additions

--- dictFunc.cpp
+++ dictFunc.cpp
@@ -3,12 +3,30 @@
   Copyright (C) 2013 Zach Brantmeier
   
 */
+//#include <stdio.h>
+//#include <iostream>
+#include <string>
+#define ARRAY_SIZE 100
+
 bool isAdjective(string checkFor){
-  return false;
+  char adjectives[ARRAY_SIZE][ARRAY_SIZE];
+  strcpy(adjectives[0],"funny");
+  strcpy(adjectives[1],"intelligent");
+  strcpy(adjectives[2],"smart");
+  return 0;
 }
 bool isName(string checkFor){
-  return false;
+  char names[ARRAY_SIZE][ARRAY_SIZE];
+  strcpy(names[0],"adam");
+  strcpy(names[1],"steve");
+  strcpy(names[2],"zach");
+  return 0;
 }
 bool isNoun(string checkFor){
-  return false;
+  char nouns[ARRAY_SIZE][ARRAY_SIZE];
+  strcpy(nouns[0],"cat");
+  strcpy(nouns[1],"dog");
+  strcpy(nouns[2],"pencil");
+  return 0;
 }
+
