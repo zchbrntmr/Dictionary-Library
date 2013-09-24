452c01f6332aa3d599768157b83e29755759bcf7
Zach <brantmeierz@ww-whippets.org>
2013-09-23 22:26:44 -0500

    Update dictFunc.js

--- dictFunc.js
+++ dictFunc.js
@@ -11,32 +11,44 @@
 
 */
 function isNoun(checkFor){
+  var found=false;
   var nouns=["apple","banana","car","cat","dog","elephant","pencil","ruler","xylophone","yacht","zebra","zygote"];
   for(var i=0;i<=nouns.length;i++){
     if(nouns[i]==checkFor.toLowerCase()){
-      return true;
-    }else{
-      return false;
+      found=true;
     }
+  }
+  if(found==true){
+    return true;
+  }else{
+    return false;
   }
 }
 function isAdjective(checkFor){
+  var found=false;
   var adjectives=["angry","bad","creative","deceptive","funny","helpful","intelligent","interesting","mean","peaceful","smart","yellow"];
   for(var i=0;i<=adjectives.length;i++){
     if(adjectives[i]==checkFor.toLowerCase()){
-      return true;
-    }else{
-      return false;
+      found=true;
     }
+  }
+  if(found==true){
+    return true;
+  }else{
+    return false;
   }
 }
 function isName(checkFor){
+  var found=false;
   var names=["alex","andrew","amy","bill","jack","jake","justin","lily","marcus","mark","matt","matthew","steve","steven","will","william","zach","zachary"];
   for(var i=0;i<=names.length;i++){
     if(names[i]==checkFor.toLowerCase()){
-      return true;
-    }else{
-      return false;
+      found=true;
     }
+  }
+  if(found==true){
+    return true;
+  }else{
+    return false;
   }
 }
