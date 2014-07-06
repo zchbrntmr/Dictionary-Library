750b6de98dff177f4252efbbb95c0e6d730bd9aa
Zach <brantmeierz@ww-whippets.org>
2014-07-06 16:22:01 -0500

    Update dictFunc.java
    Large efficiency improvements and some fixes.

--- dictFunc.java
+++ dictFunc.java
@@ -1,34 +1,28 @@
 public boolean isAdjective(String checkFor){
-  String checkForLC=checkFor.toLowerCase();
   String[] adjectives={"amazing","cold","funny","smart"};
-  for(int i=-1;i<=adjectives.length;i++){
-    if(adjectives[i]==checkForLC){
+  for(int i=0;i<=adjectives.length;i++){
+    if(adjectives[i]==checkFor.toLowerCase()){
       return true;
-    }else{
-      return false;
     }
   }
+  return false;
 }
 public boolean isName(String checkFor){
-  String checkForLC=checkFor.toLowerCase();
   String[] names={};
-  for(int i=-1;i<=names.length;i++){
-    if(names[i]==checkForLC){
+  for(int i=0;i<=names.length;i++){
+    if(names[i]==checkFor.toLowerCase(){
       return true;
-    }else{
-      return false;
     }
   }
+  return false;
 }
 public boolean isNoun(String checkFor){
-  String checkForLC=checkFor.toLowerCase();
   String[] nouns={};
-  for(int i=-1;i<=nouns.length;i++){
-    if(nouns[i]==checkForLC){
+  for(int i=0;i<=nouns.length;i++){
+    if(nouns[i]==checkFor.checkFor()){
       return true;
-    }else{
-      return false;
     }
   }
+  return false;
 }
 
