6e5093fa74f3c9060bdce629ea8166e9a790fc93
Zach <brantmeierz@ww-whippets.org>
2014-07-06 16:18:40 -0500

    Update dictFunc.js
    Removed some unnecessary parts and replaced with much more efficient and shorter code.  Also converted functions into string methods in order to prevent type errors.

--- dictFunc.js
+++ dictFunc.js
@@ -1,54 +1,33 @@
 /*
 
-  Copyright (C) 2013 Zach Brantmeier
-  
-*/
-
-/*
-
-  The functions below can be used as conditionals to check/compare strings
+  The methods below can be used with conditionals to check/compare strings
   For instance, to check if a word is a name or not in order to determine the correct response
 
 */
-function isNoun(checkFor){
-  var found=false;
+String.prototype.isNoun=function(checkFor){
   var nouns=["apple","banana","car","cat","dog","elephant","pencil","ruler","xylophone","yacht","zebra","zygote"];
   for(var i=0;i<=nouns.length;i++){
     if(nouns[i]==checkFor.toLowerCase()){
-      found=true;
+      return true;
     }
   }
-  if(found==true){
-    return true;
-  }else{
-    return false;
-  }
+  return false;
 }
-function isAdjective(checkFor){
-  var found=false;
+String.prototype.isAdjective=function(checkFor){
   var adjectives=["angry","bad","creative","deceptive","funny","helpful","intelligent","interesting","mean","peaceful","smart","yellow"];
   for(var i=0;i<=adjectives.length;i++){
     if(adjectives[i]==checkFor.toLowerCase()){
-      found=true;
+      return true;
     }
   }
-  if(found==true){
-    return true;
-  }else{
-    return false;
-  }
+  return false;
 }
-function isName(checkFor){
-  var found=false;
+String.prototype.isName=function(checkFor){
   var names=["alex","andrew","amy","bill","jack","jake","justin","lily","marcus","mark","matt","matthew","steve","steven","will","william","zach","zachary"];
   for(var i=0;i<=names.length;i++){
     if(names[i]==checkFor.toLowerCase()){
-      found=true;
+      return true;
     }
   }
-  if(found==true){
-    return true;
-  }else{
-    return false;
-  }
+  return false;
 }
